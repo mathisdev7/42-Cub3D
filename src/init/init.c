3c1996f007540da0e3e7a3d28e4550f7498098ce
mathisdev7 <999frostz@gmail.com>
2025-02-09 10:48:40 +0100

    fix(cords): fix player cords

--- src/init/init.c
+++ src/init/init.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nopareti <nopareti@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mazeghou <mazeghou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 04:33:25 by nopareti          #+#    #+#             */
-/*   Updated: 2025/02/09 02:25:03 by nopareti         ###   ########.fr       */
+/*   Updated: 2025/02/09 10:47:13 by mazeghou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,8 +52,9 @@ void	init_screen_buffer(t_game *game)
 
 int	init_textures(t_game *game)
 {
-	char	*texture_paths[4] = {"./assets/eagle.xpm", "./assets/purplestone.xpm",
-			"./assets/red.xpm", "./assets/greystone.xpm"};
+	char	*texture_paths[4] = {"./assets/eagle.xpm",
+			"./assets/purplestone.xpm", "./assets/red.xpm",
+			"./assets/greystone.xpm"};
 	int		i;
 	int		width;
 	int		height;
@@ -92,12 +93,12 @@ t_game	*init_game(t_map_info map_info)
 	game->mlx = mlx_init();
 	game->win = mlx_new_window(game->mlx, game->screen_width,
 			game->screen_height, "Cub3d");
-	game->player_pos_x = 2 + 0.5;
-	game->player_pos_y = 1 + 0.5;
-	game->player_dir_x = 0;
-	game->player_dir_y = -1;
-	game->plane_x = 0.66;
-	game->plane_y = 0;
+	game->player_pos_x = map_info.player_x + 0.5;
+	game->player_pos_y = map_info.player_y + 0.5;
+	game->player_dir_x = map_info.player_dir_x;
+	game->player_dir_y = map_info.player_dir_y;
+	game->plane_x = map_info.player_plane_x;
+	game->plane_y = map_info.player_plane_y;
 	game->player_move_x = 0;
 	game->player_move_y = 0;
 	game->player_rotate = 0;
